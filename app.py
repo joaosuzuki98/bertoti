3041829dd973ba106ffa832b0b49ede3a25edbfa
Bertoti <Giuliano@users.noreply.huggingface.co>
2025-02-16 02:01:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[DuckDuckGoSearchTool(), image_generation_tool], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
